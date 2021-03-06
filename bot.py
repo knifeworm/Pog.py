b8b8378f6b1ee29aa334e4ac5dcc95a737d5b3cc
knifeworm <knifeworm213133@gmail.com>
2021-03-06 12:20:49 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,31 @@
 import discord
 from discord.ext import commands
 import os
+
+bot = commands.Bot(command_prefix = "!")
+
+@bot.event
+async def on_ready():
+    print("Pog.py is online!")
+
+
+@bot.command()
+@commands.has_guild_permissions(administrator=True)
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send(f"Cog {extension} has been loaded! :white_check_mark:")
+
+
+@bot.command()
+@commands.has_guild_permissions(administrator=True)
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    await ctx.send(f"Cog {extension} has been unloaded! :white_check_mark:")
+
+@bot.command()
+@commands.has_guild_permissions(administrator=True)
+async def reload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send(f"Cog {extension} has been reloaded! :white_check_mark:")
+

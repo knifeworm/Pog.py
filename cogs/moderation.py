e1ed849f9e0316d702be6eb6906677761df26e21
knifeworm <knifeworm213133@gmail.com>
2021-03-06 12:50:40 +0000

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -32,7 +32,7 @@ class moderation(commands.Cog):
     
     @commands.command()
     @commands.has_guild_permissions(kick_members=True)
-    async def kick(self, ct, member : discord.Member, *, reason=None):
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
         embed = discord.Embed(title="Kick")
         embed.add_field(name="Member Kicked", value=f"{member}", inline=False)
         embed.add_field(name="Staff Member", value=f"{ctx.author}", inline=False)
@@ -44,6 +44,68 @@ class moderation(commands.Cog):
         mem.add_field(name="Server", value=f"{ctx.guild.name}", inline=False)
         await member.send(embed=mem)
         await member.kick(reason=reason)
+
+    @commands.command()
+    @commands.has_guild_permissions(mute_members=True)
+    async def mute(self, ctx, member : discord.Member, *, reason=None):
+        role = discord.utils.get(ctx.guild.roles, name="Muted")
+
+        if role is None:
+
+            await ctx.send("Muted role not found i am creating the role for you!")
+            create_role = await ctx.guild.create_role(name="Muted", reason = "Created for the mute command!")
+            await ctx.send("Setting permissions!")
+
+            for channel in ctx.guild.channels:
+                role_channel = discord.utils.get(ctx.guild.roles, name="Muted")
+                await channel.set_permissions(role_channel, speak=False, send_messages=False, read_message_history=True, read_messages=True)
+                await ctx.send("Permissions set!")
+                await ctx.send("Muteing!")
+                await member.add_roles(role_channel)
+                embed = discord.Embed(title = "Mute")
+                embed.add_field(name = "Member Muted", value = f'{member}', inline=False)
+                embed.add_field(name = "Staff Member", value = f'{ctx.author}', inline=False)
+                embed.add_field(name = "Reason", value = f'{reason}', inline=False)
+                await ctx.send(embed = embed)
+                mem = discord.Embed(title = "Mute")
+                mem.add_field(name = "Staff Member", value = f'{ctx.author}', inline=False)
+                mem.add_field(name = "Reason", value = f'{reason}', inline=False)
+                mem.add_field(name = "Server", value = f'{ctx.guild.name}', inline=False)
+                await member.send(embed = mem)
+                break
+                if role_channel in member.roles:
+                    await ctx.send(f"{member} is already muted :x:")
+                    break
+        else:
+            role_channell = discord.utils.get(ctx.guild.roles, name="Muted")
+            await member.add_roles(role_channell)
+            embed = discord.Embed(title = "Mute")
+            embed.add_field(name = "Member Muted", value = f'{member}', inline=False)
+            embed.add_field(name = "Staff Member", value = f'{ctx.author}', inline=False)
+            embed.add_field(name = "Reason", value = f'{reason}', inline=False)
+            await ctx.send(embed = embed)
+            mem = discord.Embed(title = "Mute")
+            mem.add_field(name = "Staff Member", value = f'{ctx.author}', inline=False)
+            mem.add_field(name = "Reason", value = f'{reason}', inline=False)
+            mem.add_field(name = "Server", value = f'{ctx.guild.name}', inline=False)
+            await member.send(embed = mem)
+
+
+    @commands.command()
+    @commands.has_guild_permissions(mute_members=True)
+    async def unmute(self, ctx, member : discord.Member):
+        role = discord.utils.get(ctx.guild.roles, name="Muted")
+        embed = discord.Embed(title = "Unmute")
+        embed.add_field(name = "Member Unmuted", value = f'{member}', inline=False)
+        embed.add_field(name = "Staff Member", value = f'{ctx.author}', inline=False)
+        await ctx.send(embed = embed)
+        mem = discord.Embed(tile = 'Unmute')
+        mem.add_field(name = "Staff Member", value = f'{ctx.author}', inline=False)
+        mem.add_field(name = "Server", value = f'{ctx.guild.name}', inline=False)
+        await member.send(embed = mem)
+        await member.remove_roles(role)
+        if role not in member.roles:
+            await ctx.send(f"{member} is not muted :x:")
 
 
 def setup(bot):
